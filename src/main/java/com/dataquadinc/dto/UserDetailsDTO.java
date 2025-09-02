892da87dee2b7c85fa0514187ee87a83642e7b0f
SasiKDP <sasank9110@gmail.com>
2025-09-02 20:08:17 +0530

    added the login flag in user details

---
+++ src/main/java/com/dataquadinc/dto/UserDetailsDTO.java
@@ -0,0 +1,43 @@
+package com.dataquadinc.dto;
+
+import java.time.LocalDateTime;
+
+public class UserDetailsDTO {
+    private String userId;
+    private String userName;
+    private String email;
+    private LocalDateTime lastLoginTime;
+
+    // Getters and setters
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(LocalDateTime lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+}
