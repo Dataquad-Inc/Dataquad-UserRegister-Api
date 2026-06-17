bad775e5362de72bf635b8ade5d0e758fdcb34a4
issach315 <issachemani227@gmail.com>
2026-06-17 19:23:25 +0530

    resovled conflit

--- src/main/java/com/dataquadinc/repository/UserDao.java
+++ src/main/java/com/dataquadinc/repository/UserDao.java
@@ -48,33 +48,6 @@ public interface UserDao extends JpaRepository<UserDetails, String>, JpaSpecific
     UserDetails findByEmail(String email);
 
     UserDetails findByUserId(String userId);
-<<<<<<< Updated upstream
-    @Query("SELECT DISTINCT u FROM UserDetails u JOIN u.roles r " +
-            "WHERE (:userId IS NULL OR u.userId = :userId) " +
-            "AND (:roleEnum IS NULL OR r.name = :roleEnum) " +
-            "AND u.entity = :entity " +
-            "AND u.status = 'ACTIVE' AND u.designation <> 'testuser'")
-    List<UserDetails> findByUserIdAndRole(@Param("userId") String userId,
-                                          @Param("roleEnum") UserType roleEnum,
-                                          @Param("entity") String entity);
-
-    @Query("SELECT DISTINCT u FROM UserDetails u " +
-            "WHERE (:userId IS NULL OR u.userId = :userId) " +
-            "AND u.entity = :entity " +
-            "AND u.status = 'ACTIVE' AND u.designation <> 'testuser' " +
-            "AND (:excludeRole IS NULL OR NOT EXISTS (" +
-            "   SELECT 1 FROM u.roles r2 WHERE r2.name = :excludeRole" +
-            "))")
-    List<UserDetails> findByUserIdAndRoleNot(@Param("userId") String userId,
-                                             @Param("excludeRole") UserType excludeRole,
-                                             @Param("entity") String entity);
-
-    @Query("SELECT u FROM UserDetails u " +
-            "WHERE u.entity = :entity " +
-            "AND u.status = 'ACTIVE' AND u.designation <> 'testuser'")
-    List<UserDetails> findAllActiveNonTestUsersByEntity(@Param("entity") String entity);
-=======
->>>>>>> Stashed changes
 
     @Query("SELECT DISTINCT u FROM UserDetails u JOIN u.roles r " +
             "WHERE (:userId IS NULL OR u.userId = :userId) " +
@@ -282,6 +255,7 @@ public interface UserDao extends JpaRepository<UserDetails, String>, JpaSpecific
     String getTeamLeadName(
             @Param("teamLeadId") String teamLeadId
     );
+
     @Query("""
        SELECT u
        FROM UserDetails u
